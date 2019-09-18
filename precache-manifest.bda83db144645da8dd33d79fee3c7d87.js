39ac6e88ffb02a5d170415a22dd580bfd3dd506f
Christer Johansson <christer.johansson@stenungsund.nu>
2019-09-18 20:04:10 +0200

    Updates

---
+++ precache-manifest.bda83db144645da8dd33d79fee3c7d87.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f848bc7e9eba75fc5eb59c85d69e1df6",
+    "url": "/personel-ledger/index.html"
+  },
+  {
+    "revision": "93f8ebafe9fa314ff83c",
+    "url": "/personel-ledger/static/css/2.c98029e8.chunk.css"
+  },
+  {
+    "revision": "5043783d024a04607e68",
+    "url": "/personel-ledger/static/css/main.bd8b06e2.chunk.css"
+  },
+  {
+    "revision": "93f8ebafe9fa314ff83c",
+    "url": "/personel-ledger/static/js/2.119639a9.chunk.js"
+  },
+  {
+    "revision": "5043783d024a04607e68",
+    "url": "/personel-ledger/static/js/main.f9bc4efa.chunk.js"
+  },
+  {
+    "revision": "8a87555410110926c5ff",
+    "url": "/personel-ledger/static/js/runtime~main.a01c0b1c.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/personel-ledger/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/personel-ledger/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/personel-ledger/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/personel-ledger/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/personel-ledger/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/personel-ledger/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/personel-ledger/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/personel-ledger/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/personel-ledger/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/personel-ledger/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/personel-ledger/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/personel-ledger/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/personel-ledger/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/personel-ledger/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/personel-ledger/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/personel-ledger/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
