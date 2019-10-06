7260921b5929c74d1ac8d4523606be2eef6a7d61
Kayvan You <kayvan_y@yahoo.com>
2019-10-06 09:12:17 +0200

    There are changes and the solution for the first. The changes are for the following items: 1)The output must contain several "persons" instead of one. So the page must show several cards beside each other. You can use "map" method to do that. 2)For each person, show the name in the correct format. It means that the first letter of the first name and last name should be capitalized. 3) Add also the address for each person in their cards.

--- src/components/FetchRandomUser.js
+++ src/components/FetchRandomUser.js
@@ -1,57 +1,68 @@
 import React, { Component } from "react";
 
 export default class FetchRandomUser extends Component {
-    // Default states for component is set here
-    state = {
-        loading: true,
-        person: null
+  // Default states for component is set here
+  state = {
+    loading: true,
+    person: []
+  };
+
+  // Asynchronus function that fecth data from an api and put in a person object.
+  async componentDidMount() {
+    const url = "https://api.randomuser.me/?results=30";
+    const response = await fetch(url);
+    const data = await response.json();
+    this.setState({ person: data.results, loading: false });
+  }
+
+  // This is the rendering function of the component, this is what we output to the screen.
+  render() {
+    if (!this.state.person) {
+      return <div>didn't get a person</div>;
     }
 
-    // Asynchronus function that fecth data from an api and put in a person object.
-    async componentDidMount() {
-        const url = "https://api.randomuser.me/?results=10";
-        const response = await fetch(url);
-        const data = await response.json();
-        this.setState({ person: data.results[0], loading: false });
-    }
-    
+    let { person: allpersons } = this.state;
 
-    // This is the rendering function of the component, this is what we output to the screen.
-    render() {
-        // The if-statement checks if there is data in the object holder or not, and displays data if there is.
-        if (this.state.loading) {
-            return <div>loading...</div>;
-        }
-
-        if (!this.state.person) {
-            return <div>didn't get a person</div>;
-        }
-        return (
-            // This is what we return to the screen, using Semantic UI CSS.
-            <div> 
-                <header>
-                    <h1>Personel Legder of ACME Inc.</h1>
-                    </header>             
-                <div class="ui card">
-                    <div class="image">
-                    <img src={this.state.person.picture.large} alt={this.state.person.name.first} />
-                    </div>
-                    <div class="content">
-                        <a class="header">{this.state.person.name.title} {this.state.person.name.first} {this.state.person.name.last}</a>
-                        <div class="meta">
-                            <span class="date">Joined in {this.state.person.registered.date}</span>
-                        </div>
-                        <div class="description">
-                        {this.state.person.name.first} {this.state.person.name.last} is an art director living in New York.
-                    </div>
-                    <div className="mail"><i className="user mail"></i> {this.state.person.email}</div>
-                    </div>
-                    <div class="extra content">
-                        <a> Country of origin: {this.state.person.nat}
-                        </a>
-                    </div>
+    return (
+      // This is what we return to the screen, using Semantic UI CSS.
+      <div>
+        <header>
+          <h1>Personel Legder of ACME Inc.</h1>
+        </header>
+        <div className="ui five column grid">
+          {allpersons.map(person => (
+            <div className="column" style={{ textTransform: "capitalize" }}>
+              <div class="ui fluid card">
+                <div class="image">
+                  <img src={person.picture.large} alt={person.name.first} />
                 </div>
+                <div class="content">
+                  <a class="header">
+                    {person.name.title} {person.name.first} {person.name.last}
+                  </a>
+                  <div class="meta">
+                    <span class="date">Joined in {person.registered.date}</span>
+                  </div>
+                  <div class="description">
+                    {person.name.first} {person.name.last} is an art director
+                    living in New York.
+                  </div>
+                  <div className="mail">
+                    <i className="user mail"></i> {person.email}
+                  </div>
+                </div>
+                <div class="extra content">
+                  <a> Country of origin: {person.nat}</a>
+                </div>
+                <div className="extra content">
+                  Address: {person.location.street.name}, {person.location.city}{" "}
+                  {person.location.state} {person.location.postcode}{" "}
+                </div>
+              </div>
             </div>
-        );
-    }
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
